001db3aba9f133b8be1e0c1df45800a49139a593
Serski <159945982+Serski@users.noreply.github.com>
2026-02-04 22:24:33 +0200

    Add service worker caching

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'empire-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/site.css',
+  '/assets/js/site.js',
+  '/manifest.json',
+  '/favicon.ico',
+  '/assets/img/og-default.webp',
+  '/assets/img/hero.webp',
+  '/assets/img/logo-eagle.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+          return null;
+        })
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) return cachedResponse;
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type === 'opaque') {
+              return response;
+            }
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, responseClone));
+            return response;
+          });
+      })
+      .catch(() => caches.match('/index.html'))
+  );
+});
